931e8c390ef44495700bfe4ee30a6da33d9185c0
YYYLIN <763089304@qq.com>
2018-03-21 00:03:44 +0800

    Signed-off-by: YYYLIN <763089304@qq.com>

--- Magic_Engine/Core/Magic_Engine/System/Supervisor.cpp
+++ Magic_Engine/Core/Magic_Engine/System/Supervisor.cpp
@@ -6,35 +6,32 @@ namespace Magic
 	{
 		void ObjectUpdataSystem::Update(EntityX::EntityManager &_es, EntityX::EventManager &_events, EntityX::TimeDelta _time)
 		{
-/*
 			EntityX::ComponentHandle<ObjectSupervisor> _ObjectSupervisor;
 			EntityX::ComponentHandle<UpdataComponent> _UpdataComponent;
 			for (EntityX::Entity _entity : _es.entities_with_components<UpdataComponent>(_UpdataComponent))
 			{
 				if (_UpdataComponent->m_Call_Updata)
 					_UpdataComponent->m_Call_Updata();
+				if (_entity.has_component<ObjectSupervisor>())
+					_entity.GetComponent<ObjectSupervisor>()->m_Supervisor.m_systems.Update<ObjectUpdataSystem>(_time);
 			}
-			for (EntityX::Entity _entity : _es.entities_with_components<ObjectSupervisor>(_ObjectSupervisor))
-			{
-				_ObjectSupervisor->m_Supervisor.m_systems.Update<ObjectUpdataSystem>(_time);
-			}*/
 		}
 
 		void ObjectRenderSystem::Update(EntityX::EntityManager &_es, EntityX::EventManager &_events, EntityX::TimeDelta _time)
 		{
-/*
-			EntityX::ComponentHandle<ObjectSupervisor> _ObjectSupervisor;
-			EntityX::ComponentHandle<RenderComponent> _RenderComponent;
-			for (EntityX::Entity _entity : _es.entities_with_components<ObjectSupervisor, RenderComponent>(_ObjectSupervisor, _RenderComponent))
-			{
-				if (_RenderComponent->m_Call_RenderStart)
-					_RenderComponent->m_Call_RenderStart();
-				_ObjectSupervisor->m_Supervisor.m_systems.Update<ObjectRenderSystem>(_time);
-				if (_RenderComponent->m_Call_Render)
-					_RenderComponent->m_Call_Render();
-				if (_RenderComponent->m_Call_RenderEnd)
-					_RenderComponent->m_Call_RenderEnd();
-			}*/
+			/*
+						EntityX::ComponentHandle<ObjectSupervisor> _ObjectSupervisor;
+						EntityX::ComponentHandle<RenderComponent> _RenderComponent;
+						for (EntityX::Entity _entity : _es.entities_with_components<ObjectSupervisor, RenderComponent>(_ObjectSupervisor, _RenderComponent))
+						{
+							if (_RenderComponent->m_Call_RenderStart)
+								_RenderComponent->m_Call_RenderStart();
+							_ObjectSupervisor->m_Supervisor.m_systems.Update<ObjectRenderSystem>(_time);
+							if (_RenderComponent->m_Call_Render)
+								_RenderComponent->m_Call_Render();
+							if (_RenderComponent->m_Call_RenderEnd)
+								_RenderComponent->m_Call_RenderEnd();
+						}*/
 		}
 
 		bool Supervisor::Initialize()
