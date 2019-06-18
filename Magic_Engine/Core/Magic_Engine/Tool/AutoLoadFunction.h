efed585196c30c6fc0791f570852b1be62771e18
LH <729370500@qq.com>
2019-06-18 10:00:02 +0800

    修改优化自动函数 Signed-off-by: LH <729370500@qq.com>

--- Magic_Engine/Core/Magic_Engine/Tool/AutoLoadFunction.h
+++ Magic_Engine/Core/Magic_Engine/Tool/AutoLoadFunction.h
@@ -7,18 +7,17 @@
 
 static std::map<std::string, std::vector<std::function<void(void)>>> S_map_auto_function;
 
-#define _LOAD_FUNCTION_(N, C, F)	class LOAD_FUNCTION_##N {	\
-								LOAD_FUNCTION_##N(){\
-									auto _auto = S_map_auto_function.find(#C);\
-									if (_auto != S_map_auto_function.end()) {\
-										_auto->second.push_back(F);\
-									}		\
-									else {	\
-										S_map_auto_function.insert(std::make_pair(#C, F)); \
-									}		\
-								}			\
-							};\
-const static LOAD_FUNCTION_##N S_LOAD_FUNCTION_##N;
+#define _LOAD_FUNCTION_(N, C, F)	static int LOAD_FUNCTION_##N = [](){\
+										auto _auto = S_map_auto_function.find(#C); \
+										if (_auto != S_map_auto_function.end()) {  \
+											_auto->second.push_back(F); \
+										}		\
+										else {	\
+											S_map_auto_function.insert(std::make_pair(#C, F)); \
+										}		\
+										return 0; \
+									}
+			
 
 #define LOAD_FUNCTION(C, F)	_LOAD_FUNCTION_(__COUNTER__, C, F)
 
